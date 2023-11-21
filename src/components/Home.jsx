c1c232076d2ee204e7593407cb9d9d60991fc969
Tonny-G <107466380+Tonny-G@users.noreply.github.com>
2023-11-21 10:03:47 +0300

    home button config

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,11 +1,12 @@
 import React from 'react'
 import {  HiArrowNarrowRight } from 'react-icons/hi';
 import Navbar from './Navbar';
+import { Link } from 'react-scroll';
 
 const Home = () => {
   return (
-    <div name='home' className="w-full h-screen bg-[#0a192f]">
-    <Navbar/>
+    <div name="home" className="w-full h-screen bg-[#0a192f]">
+      <Navbar />
       {/*container */}
       <div className="max-w-[1000px] mx-auto px-8 flex flex-col justify-center h-full">
         <p className="text-yellow-600 text-xl">Hi, my name is</p>
@@ -16,18 +17,20 @@ const Home = () => {
           I am a Full Stack developer
         </h2>
         <p className="text-[#8892b0] py-4 max-w-[700px]">
-          I am a full-stack developer specializing in building (and
-          occasionally designing) exceptional web applications and other digital
-          experiences. I am currently focused on building responsive full-stack
-          web applications
+          I am a full-stack developer specializing in building (and occasionally
+          designing) exceptional web applications and other digital experiences.
+          I am currently focused on building responsive full-stack web
+          applications
         </p>
         <div>
-          <button className="text-white group border-2 py-4 px-3 my-2 flex items-center hover:bg-yellow-600 hover:border-yellow-600">
-            View work
-            <span className='ml-3 flex group-hover:rotate-90 duration-300 '>
-              <HiArrowNarrowRight />
-            </span>
-          </button>
+          <Link to='work' smooth={true} duration={500}>
+            <button className="text-white group border-2 py-4 px-3 my-2 flex items-center hover:bg-yellow-600 hover:border-yellow-600">
+              View work
+              <span className="ml-3 flex group-hover:rotate-90 duration-300 ">
+                <HiArrowNarrowRight />
+              </span>
+            </button>
+          </Link>
         </div>
       </div>
     </div>
